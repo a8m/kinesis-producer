da1db9a32692d3aeb8bed161484d1c111154a1b2
Owais Lone <hello@owaislone.org>
2018-11-22 03:24:48 +0530

    Re-structured popular implementations

--- loggers/kplogrus/logrus.go
+++ loggers/kplogrus/logrus.go
@@ -1,26 +1,26 @@
-package loggers
+package kplogrus
 
 import (
 	producer "github.com/a8m/kinesis-producer"
 	"github.com/sirupsen/logrus"
 )
 
-// Logrus implements a logurs.Logger for kinesis-producer
-type Logrus struct {
+// Logger implements a logurs.Logger logger for kinesis-producer
+type Logger struct {
 	Logger *logrus.Logger
 }
 
 // Info logs a message
-func (l *Logrus) Info(msg string, args ...producer.LogValue) {
+func (l *Logger) Info(msg string, args ...producer.LogValue) {
 	l.Logger.WithFields(l.valuesToFields(args...)).Info(msg)
 }
 
 // Error logs an error
-func (l *Logrus) Error(msg string, err error, args ...producer.LogValue) {
+func (l *Logger) Error(msg string, err error, args ...producer.LogValue) {
 	l.Logger.WithError(err).WithFields(l.valuesToFields(args...)).Error(msg)
 }
 
-func (l *Logrus) valuesToFields(values ...producer.LogValue) logrus.Fields {
+func (l *Logger) valuesToFields(values ...producer.LogValue) logrus.Fields {
 	fields := logrus.Fields{}
 	for _, v := range values {
 		fields[v.Name] = v.Value
--- loggers/kpzap/zap.go
+++ loggers/kpzap/zap.go
@@ -1,4 +1,4 @@
-package loggers
+package kpzap
 
 import (
 	"go.uber.org/zap"
@@ -6,24 +6,24 @@ import (
 	producer "github.com/a8m/kinesis-producer"
 )
 
-// Zap implements a logurs.Logger for kinesis-producer
-type Zap struct {
+// Logger implements a zap.Logger logger for kinesis-producer
+type Logger struct {
 	Logger *zap.Logger
 }
 
 // Info logs a message
-func (l *Zap) Info(msg string, values ...producer.LogValue) {
+func (l *Logger) Info(msg string, values ...producer.LogValue) {
 	l.Logger.Info(msg, l.valuesToFields(values)...)
 }
 
 // Error logs an error
-func (l *Zap) Error(msg string, err error, values ...producer.LogValue) {
+func (l *Logger) Error(msg string, err error, values ...producer.LogValue) {
 	fields := l.valuesToFields(values)
 	fields = append(fields, zap.Error(err))
 	l.Logger.Info(msg, fields...)
 }
 
-func (l *Zap) valuesToFields(values []producer.LogValue) []zap.Field {
+func (l *Logger) valuesToFields(values []producer.LogValue) []zap.Field {
 	fields := make([]zap.Field, len(values))
 	for i, v := range values {
 		fields[i] = zap.Any(v.Name, v.Value)
